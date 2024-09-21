0f92d737c23a6892d82ada7389f0d945584e6661
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-09-21 14:05:43 +0300

    Add sum function

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,10 @@
 public class Calculator {
     public static void main(String[]args){
-
+        Scanner scanner = new Scanner(System.in);
+        
+        int firstNumber = Integer.ParseInt(scanner.nextLine);
+        int SecondNumber = Integer.ParseInt(scanner.nextLine);
+        
+        System.out.println(firstNumber + SecondNumber);
     }
 }
